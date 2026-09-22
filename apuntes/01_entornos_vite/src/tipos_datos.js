f25ba66898c73f07b966a7cec8edcd7b599265d6
oscxxrrr <martinez8cabrera2007@gmail.com>
2026-09-22 10:17:01 +0200

    ejercicios por hacer

---
+++ apuntes/01_entornos_vite/src/tipos_datos.js
@@ -0,0 +1,38 @@
+//  EJ_CLASE:
+//
+// Funcion que le pase como parametro un numero en grados celsius y lo transforma a grados kelvin.
+//v1
+function celsiusToKelvin(celsius){
+  let kelvin = celsius + 273.15
+  return kelvin
+}
+
+//v2
+function celsiusToKelvin2(celsius){
+  return celsius + 273.15
+}
+
+//v3
+const celToKel = (celsius) => {
+  return celsius + 273.15
+}
+
+//v4
+const cToK = ( c ) => c + 273.15
+
+// funcion que le pase como parametro 2 numeros y me los ordene
+
+
+
+
+// funcion que pase celsius a kelvin pero comprobando q celsius es un numero, 
+// que la temperatura no puede ser inferior a 0 absoluto. (-273)
+// y el resultado dado con solamente 2 cifra decimal
+//
+// isNaN <-- comprobar q no es un numero o que si
+// ¿como hacer q salga solamente 2 decimales?
+
+
+
+
+
